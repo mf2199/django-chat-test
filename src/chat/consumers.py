128a67b59266b3193bcb0be33f6d7c5d366e58a8
STATION\MF <mf2199@columbia.edu>
2023-02-19 16:17:14 -0500

    basic styling

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -1,44 +1,96 @@
 import json
 
-from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth import get_user_model
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+
+from .models import Message
+
+user = get_user_model()
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
+class ChatConsumer(WebsocketConsumer):
     def __init__(self):
         super().__init__()
         self.room_name = None
         self.room_group_name = None
 
-    async def connect(self):
+    def fetch_messages(self, data):
+        messages = Message.last_messages()
+        content = {
+            "messages": self.messages_to_json(messages)
+        }
+        self.send_message(content)
+
+    def messages_to_json(self, messages):
+        result = []
+        for message in messages:
+            result.append(
+                {
+                    "author": message.author.username,
+                    "content": message.content,
+                    "timestamp": str(message.timestamp),
+                }
+            )
+
+        return result
+
+    def new_message(self, data):
+        author = data["from"]
+        author_user = user.objects.filter(username=author)[0]
+        message = Message.objects.create(
+            author=author_user, content=data["message"]
+        )
+        content = {
+            "command": "new_message",
+            "message": {
+                "author": message.author.username,
+                "content": message.content,
+                "timestamp": str(message.timestamp),
+            }
+        }
+
+        return self.send_chat_message(content)
+
+    commands = {
+        "fetch_messages": fetch_messages,
+        "new_message": new_message,
+    }
+
+    def connect(self):
+        """Join the room group."""
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
 
-        # Join room group
-        await self.channel_layer.group_add(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name, self.channel_name
         )
 
-        await self.accept()
+        self.accept()
 
-    async def disconnect(self, close_code):
-        # Leave room group
-        await self.channel_layer.group_discard(
+    def disconnect(self, close_code):
+        """Leave room group."""
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name, self.channel_name
         )
 
-    # Receive message from WebSocket
-    async def receive(self, text_data=None, bytes_data=None):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+    def receive(self, text_data=None, bytes_data=None):
+        """Receives message from WebSocket and sends message to the room group.
+        """
+        data = json.loads(text_data)
+        self.commands[data["command"]](self, data)
 
-        # Send message to room group
-        await self.channel_layer.group_send(
+    def send_chat_message(self, message):
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name, {"type": "chat_message", "message": message}
         )
 
-    # Receive message from room group
-    async def chat_message(self, event):
+    def send_message(self, message):
+        self.send(text_data=json.dumps(message))
+
+    def chat_message(self, event):
+        """Receives message from the room group and sends message to WebSocket.
+        """
         message = event["message"]
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+        self.send(text_data=json.dumps(message))
--- src/chat/models.py
+++ src/chat/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+user = get_user_model()
+
+
+class Message(models.Model):
+    author = models.ForeignKey(
+        user, related_name="author_messages", on_delete=models.CASCADE
+    )
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username
+
+    @staticmethod
+    def last_messages(limit=20):
+        return Message.objects.order_by("-timestamp").all()[:limit]
